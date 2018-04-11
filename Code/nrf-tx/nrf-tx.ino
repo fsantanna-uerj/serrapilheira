aa9e8c50d77db49b4300d931fc128b8b5e6c7d3b
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-04-11 12:07:49 -0300

    (+) Código NRF

---
+++ Code/nrf-tx/nrf-tx.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+byte V = 0;
+const uint64_t pipe =  0xAABBCCDDEELL;
+RF24 radio(8,7);
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+    radio.begin();
+    //radio.disableCRC();
+    radio.setPayloadSize(1);
+    radio.setAutoAck(false);              // true by default
+    radio.setDataRate(RF24_2MBPS);
+    radio.openWritingPipe(pipe);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    delay(1000);
+    Serial.print("send ");
+    Serial.println(V);
+    radio.write(&V, 1);
+    V++;
+}
